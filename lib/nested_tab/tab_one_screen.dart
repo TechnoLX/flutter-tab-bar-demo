faf25077437bf1d5a481d83b35d58f0c8fc7900f
TechnoLX <78125150+TechnoLX@users.noreply.github.com>
2021-02-25 21:21:05 +0800

    Update tab_one_screen.dart

--- lib/nested_tab/tab_one_screen.dart
+++ lib/nested_tab/tab_one_screen.dart
@@ -1,30 +1,58 @@
-import 'package:NewApp/tab_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MyApp());
+class TabOneScreen extends StatefulWidget {
+  TabOneScreen({Key key}) : super(key: key);
+
+  @override
+  _TabOneScreenState createState() => _TabOneScreenState();
 }
 
-class MyApp extends StatelessWidget {
+//this tab one screen has bottom navigator tab bar
+class _TabOneScreenState extends State<TabOneScreen> {
+  int currentPage = 0; //for siwtching tab view
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Tab Bar Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        //primarySwatch: color,
-      ),
-      home: TabBarController(),
+    final List<BottomNavigationBarItem> items = [
+      BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'home'),
+      BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'settings'),
+      BottomNavigationBarItem(icon: Icon(Icons.edit), label: 'edit'),
+    ];
+    PageController _controller = PageController(
+      initialPage: currentPage,
     );
-  }
-}
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+    void onTapIcon(int index) => _controller.animateToPage(index,
+        duration: const Duration(milliseconds: 200), curve: Curves.easeIn);
+    return Scaffold(
+      body: PageView(
+        controller: _controller,
+        onPageChanged: (value) {
+          setState(() {
+            currentPage = value;
+          });
+        },
+        children: [
+          Center(
+            child: Text('bottom tab 1'),
+          ),
+          Center(
+            child: Text('bottom tab 2'),
+          ),
+          Center(
+            child: Text('bottom tab 3'),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPage,
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.redAccent,
+        unselectedItemColor: Colors.white,
+        onTap: onTapIcon,
+        //you may also wrap this items into list
+        items: items,
+      ),
+    );
   }
 }
